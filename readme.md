0db690ed87ac3a271c596062936de5ebc31c8ac3
psycoder69 <zycus69@gmail.com>
2023-04-20 15:54:01 +0530

    First Commit

---
+++ readme.md
@@ -0,0 +1,5 @@
+Hi there!
+
+This is a static LinkedIn Page Clone
+
+I'll be making it responsive very soon!
---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("search").addEventListener("click", function () {
+    alert("lmao");
+});
